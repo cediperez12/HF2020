bfa0a7d5b15cd57551d9cd53998d5dae4d64f400
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2020-11-27 23:52:19 +0800

    Nagpush ako

--- app/src/main/java/com/iced/hf2020/LoginActivity.java
+++ app/src/main/java/com/iced/hf2020/LoginActivity.java
@@ -3,13 +3,12 @@ package com.iced.hf2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
     }
 }
