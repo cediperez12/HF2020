263c81621b1bf5f814df1902a57396428392a5eb
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2020-11-27 22:11:13 +0800

    Sign up design pls.

--- app/src/main/java/com/iced/hf2020/SignUpActivity.java
+++ app/src/main/java/com/iced/hf2020/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.iced.hf2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Log.d("hello","komoesta");
     }
 }
